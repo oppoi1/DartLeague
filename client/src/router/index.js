52e371489eedff4bc1a1496723c32ba188eabff3
oppoi1 <wannemueller@pm.me>
2019-01-21 11:53:41 +0100

    404 page & route added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,6 +6,7 @@ import Login from '../components/authentication/Login'
 import Profile from '../components/Profile'
 import LeagueOverview from '../components/LeagueOverview'
 import LeagueView from '../components/LeagueView'
+import page404 from '../components/static/404'
 
 Vue.use(Router)
 
@@ -48,6 +49,28 @@ export default new Router({
       path: '/dart/de/leagues/:tId',
       name: 'leagueView',
       component: LeagueView
+    },
+    /*
+    {
+      // will match anything starting with `/user-`
+      // basically use it for most routes like leagues and users to show if something specific couldn't be found
+      path: '/user-*'
     }
+
+    // see https://router.vuejs.org/guide/essentials/dynamic-matching.html#catch-all-404-not-found-route
+    // Given a route { path: '/user-*' }
+    this.$router.push('/user-admin')
+    this.$route.params.pathMatch // 'admin'
+
+    // Given a route { path: '*' }
+    this.$router.push('/non-existing')
+    this.$route.params.pathMatch // '/non-existing'
+
+    */
+    {
+      path: '*',
+      name: page404,
+      component: page404
+    },
   ]
 })
